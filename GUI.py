c51d9c9981e29ef92935dda5be2bb03b7fa1eee4
jordantiu <47049525+jayceetee23@users.noreply.github.com>
2020-10-04 22:52:02 -0500

    GUI Board and borders

--- GUI.py
+++ GUI.py
@@ -1,34 +1,196 @@
 import pygame
 
-# Init pygame
+# TODO: References
+# https://pythonprogramming.net/pygame-start-menu-tutorial/?completed=/pygame-drawing-shapes-objects/
+# https://www.youtube.com/watch?v=jh_m-Eytq0Q&list=PLQVvvaa0QuDdLkP8MrOXLe_rKuf6r80KO&index=11&ab_channel=sentdex
+
+
 pygame.init()
 
-# Create screen with pixel height of 800 x 600
-screen = pygame.display.set_mode((800, 600))
+# Init pygame
+display_width = 800
+
+display_height = 570
+screen = pygame.display.set_mode((display_width, display_height))
+
+# Create screen with pixel length x height of 800 x 600
+pygame.display.set_caption("Sudoku Puzzle")
 
 # Title and Icon
-pygame.display.set_caption("Sudoku Puzzle")
 icon = pygame.image.load('images/sudoku.png')
 pygame.display.set_icon(icon)
+black = (0, 0, 0)
 
-# Game Loop
-running = True
-while running:
-    # Gets all events
-    for event in pygame.event.get():
-
-        # Quit sudoku game
-        if event.type == pygame.QUIT:
-            running = False
-
-        # if event.type == pygame.
+# Defined colors: RGB - Red, Green, Blue
+white = (255, 255, 255)
+red = (200, 0, 0)
+green = (0, 200, 0)
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+gray = (192, 192, 192)
+light_blue = (193, 225, 236)
 
 
+def button(text, x_coordinate, y_coordinate, button_width, button_height, color, hover_color, action=None):
+    # Mouse Position
+    mouse = pygame.mouse.get_pos()
+    # print(mouse)
+    click = pygame.mouse.get_pressed()
+    # print(click)
 
-    # RGB - Red, Green, Blue
-    screen.fill((255, 255, 255))
-    pygame.display.update()
+    # Note: mouse[0] is x coordinate of mouse; mouse[1] is y coordinate
+    # Button Hover
+    if x_coordinate < mouse[0] < x_coordinate + button_width and y_coordinate < mouse[1] < y_coordinate + button_height:
+        pygame.draw.rect(screen, hover_color, (x_coordinate, y_coordinate, button_width, button_height))
 
-    # if event.type.
+        if click[0] == 1 and action != None:
+            action()
 
-# HANG THE PROGRAM
+    else:
+        pygame.draw.rect(screen, color, (x_coordinate, y_coordinate, button_width, button_height))
+
+    # Button Text
+    button_text = pygame.font.Font("freesansbold.ttf", 20)
+    text_surf, text_rect = text_objects(text, button_text)
+    text_rect.center = ((x_coordinate + (button_width / 2)), (y_coordinate + (button_height / 2)))
+    screen.blit(text_surf, text_rect)
+
+
+def text_objects(text, font):
+    text_surface = font.render(text, True, black)
+
+    return text_surface, text_surface.get_rect()
+
+
+# Open Game Menu
+def main_menu():
+    menu = True
+
+    while menu:
+        # Gets all events
+        for event in pygame.event.get():
+
+            # Quit sudoku game
+            if event.type == pygame.QUIT:
+                menu = False
+
+        # Screen color
+        screen.fill(white)
+
+        # Note mouse[0] is x coordinate of mouse; mouse[1] is y coordinate
+        # Start button hover
+
+        button("Start", display_width / 2 - 50, 350, 100, 50, green, bright_green, start_sudoku)
+        button("Exit", display_width / 2 - 50, 450, 100, 50, red, bright_red, exit_game)
+
+        # Position away from left, position away from top, button size length (increases to right), button size height (increases downward)
+
+        # Display Game Title
+        title_text = pygame.font.Font('freesansbold.ttf', 115)
+        text_surf, text_rect = text_objects("Play Sudoku", title_text)
+        text_rect.center = ((display_width / 2), (display_height / 3))
+        screen.blit(text_surf, text_rect)
+
+        # Update screen
+        pygame.display.update()
+
+
+# TODO: Current work position 10.04.2020
+# https://www.youtube.com/watch?v=XpYz-q1lxu8&feature=emb_title
+# Sudoku game
+
+
+# NOTE: The game board is buffered from the top left hand corner by a square_size number of pixels
+square_size = 50
+column_count = 9
+row_count = 9
+
+board_width = column_count * square_size
+height = row_count * square_size
+
+
+# Draws individual sudoku squares
+def draw_board():
+    for column in range(column_count + 1):
+        # Add buffer to board
+        if column == 0:
+            continue
+        for row in range(row_count + 1):
+            # Add buffer to board
+            if row == 0:
+                continue
+            pygame.draw.rect(screen, black, (column * square_size, row * square_size, square_size, square_size), 1)
+
+
+# Draws borders in between sudoku boxes
+def draw_border():
+    pygame.draw.rect(screen, black, (square_size, square_size + square_size * 3, square_size * 9, square_size * 3), 5)
+    pygame.draw.rect(screen, black, (square_size + square_size * 3, square_size, square_size * 3, square_size * 9), 5)
+    pygame.draw.rect(screen, black, (square_size, square_size, square_size * 9, square_size * 9), 5)
+
+
+def cube(text, x_coordinate, y_coordinate, button_width, button_height, color, hover_color):
+    # Mouse Position
+    mouse = pygame.mouse.get_pos()
+    print(mouse)
+    click = pygame.mouse.get_pressed()
+    print(click)
+
+    # Note: mouse[0] is x coordinate of mouse; mouse[1] is y coordinate
+    # Button Hover
+    if x_coordinate < mouse[0] < x_coordinate + button_width and y_coordinate < mouse[1] < y_coordinate + button_height:
+        pygame.draw.rect(screen, hover_color, (x_coordinate, y_coordinate, button_width, button_height))
+
+    else:
+        pygame.draw.rect(screen, color, (x_coordinate, y_coordinate, button_width, button_height))
+
+    # Button Text
+    button_text = pygame.font.Font("freesansbold.ttf", 20)
+    text_surf, text_rect = text_objects(text, button_text)
+    text_rect.center = ((x_coordinate + (button_width / 2)), (y_coordinate + (button_height / 2)))
+    screen.blit(text_surf, text_rect)
+
+
+def start_sudoku():
+    sudoku = True
+
+    while sudoku:
+
+        # Background screen color
+        screen.fill(white)
+
+        # Draw the sudoku game board
+        draw_board()
+
+        # Draw borders for sudoku board
+        draw_border()
+
+        # Gets all events
+        for event in pygame.event.get():
+
+            # Quit sudoku game
+            if event.type == pygame.QUIT:
+                quit()
+
+            # If click
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+            # TODO: Arrow key movements
+
+            # TODO: Number input
+
+            # TODO: Backspace to undo number
+
+        # cube("2", 350, 350, 100, 50, white, light_blue)
+
+        # Update screen
+        pygame.display.update()
+
+
+def exit_game():
+    pygame.quit()
+    quit()
+
+
+main_menu()
