f4533c8c7253dc125c13c77f362804fc2678f77f
jordantiu <47049525+jayceetee23@users.noreply.github.com>
2020-10-13 19:42:08 -0500

    fix to generate unique puzzles

--- generator.py
+++ generator.py
@@ -1,7 +1,5 @@
 import numpy as np
 import random
-import validate
-import solver
 
 blank = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -83,32 +81,91 @@ def add_blanks(grid):
 
     return grid
 
-#
-# print(np.matrix(blank))
-#
+
+# TODO: Work on verifying grid has a unique solution
+def reset_grid(grid):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            grid[i][j] = 0
+
+
+count = 0
+is_unique = True
+
+def check_if_unique(grid):
+    global count
+    global is_unique
+
+    if is_unique == False:
+        return False
+
+    for y in range(0, 9):
+        for x in range(0, 9):
+            # Check if the grid is empty with '0'
+            if grid[y][x] == 0:
+                # For number choices between 1 and 9
+                for n in range(1, 10):
+                    # if valid
+                    if check_valid(y, x, n, grid):
+                        grid[y][x] = n
+
+                        # Recursive call to find next empty square
+                        check_if_unique(grid)
+
+                        # # Check if solved
+                        # if completed(grid):
+                        #     return
+
+                        # Backtracking
+                        grid[y][x] = 0
+
+                return
+
+    count = count + 1
+
+    if count > 1:
+        is_unique = False
+
+    return
+
 # fill(blank)
-#
-# reference = blank
-#
-# print(np.matrix(reference))
-#
-# print(np.matrix(blank))
-# #
 # add_blanks(blank)
-# #
-# # print(np.matrix(blank))
-# #
-#
-#
-# print(np.matrix(reference))
 # print(np.matrix(blank))
-#
-#
-# solver.solve(blank)
-#
-# print(np.matrix(blank))
-#
-# if reference == blank:
-#     print("TRUEEEE")
-# else:
-#     print("FASLE")
+# check_if_unique(blank)
+# print(is_unique)
+
+
+def generate_unique_puzzle(grid):
+    global is_unique
+    global count
+
+    building_sudoku = True
+
+    while building_sudoku:
+        fill(grid)
+        add_blanks(grid)
+        check_if_unique(grid)
+
+        if is_unique:
+            building_sudoku = False
+
+        else:
+            grid = [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+
+            is_unique = True
+            count = 0
+
+    return grid
+
+
+generate_unique_puzzle(blank)
